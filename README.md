f3fb7c7335567dffb3c795dfc52068c63d878937
CesarArathAP <122801465+CesarArathAP@users.noreply.github.com>
2023-01-24 14:57:26 +0000

    Concatenación mediante la funcion format para reaizar una suma, resta, multiplicación y división con todo y el resultado

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Repositorio con los ejercicios de la materia Programación Orientada a Objetos
 | 1| programa0.py| Hola mundo en Python3|
 | 2| programa1.py|Concatenación y comentarios multimedia.py|
 | 3| programa2.py|Concatenación mediante la función format.py|
-| 4| programa3.py|Concatenación mediante la funcion format para reaizar una suma con todo y el resultado.py|
+| 4| programa3.py|Concatenación mediante la funcion format para reaizar una suma, resta, multiplicación y división con todo y el resultado.py|
--- semana2/programa3.py
+++ semana2/programa3.py
@@ -1,4 +1,10 @@
 variable1 = 10 #  variable para alamcenar una cadena de caracteristicas
-variable2 = 5.2 #  variable para cadenas de caracteres
+variable2 = 5 #  variable para cadenas de caracteres
 print(variable1 + variable2) #  concatena caracteres e imprime los resultados
-print("{}+{}={}".format(variable1, variable2, variable1+variable2))  #concatena caracteres e imprime con funcion format y realiza la suma y al final el resultado
+print("{} + {} = {}".format(variable1, variable2, variable1+variable2))  #concatena caracteres e imprime con funcion format y realiza la suma y al final el resultado
+
+print("{} - {} = {}".format(variable1, variable2, variable1-variable2))
+
+print("{} = {} * {}".format(variable1* variable2, variable1,variable2))
+
+print("{} / {} = {}".format(variable1, variable2, variable1/variable2))
